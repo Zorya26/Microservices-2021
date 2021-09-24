ff8020c8e71e2e57921a19f2c67fbe62e2f806df
Zoryana Sobol <Zoryana.Sobol@jumar-technology.com>
2021-09-24 12:34:13 +0300

    start 4 lab

--- LoggingService/Controllers/LogginController.cs
+++ LoggingService/Controllers/LogginController.cs
@@ -11,22 +11,18 @@ namespace LoggingService.Controllers
     public class LogginController : Controller
     {
         private readonly ILogger<LogginController> _logger;
-        //private readonly IMemoryCache _memoryCache;
 
         public static Dictionary<Guid, string> messages = new Dictionary<Guid, string>();
 
         public LogginController(ILogger<LogginController> logger, IMemoryCache memoryCache)
         {
             _logger = logger;
-            //_memoryCache = memoryCache;
         }
 
         [HttpGet]
         public string GetLog()
         {
             var messagesCont = "";
-
-            //_memoryCache.TryGetValue("messages", out messages);
 
             foreach (var message in messages.Values)
             {
@@ -41,23 +37,7 @@ namespace LoggingService.Controllers
         [HttpPost]
         public string LogPost([FromBody] MessageModel message)
         {
-            //if (_memoryCache.TryGetValue("messages", out messages))
- 
-//if (messages.Values.Count != 0)
-            //{
                 messages.Add(message.Id, message.Value);
-            //}
-            
-
-            //else
-            //{
-            //    messages = new Dictionary<Guid, string>
-            //    {
-            //        { message.Id, message.Value }
-            //    };
-            //}
-            
-            //_memoryCache.Set("messages", messages);
 
             _logger.LogInformation("Request to Logging Controller");
 
